868e0e025da2b0ec56a765bcbe78cf293d792fad
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-23 17:08:36 +0400

    Create UserRepository.java

---
+++ src/main/java/com/rustam/unitech/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.rustam.unitech.repository;
+
+import com.rustam.unitech.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+}
