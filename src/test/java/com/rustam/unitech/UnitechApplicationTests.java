c51b804af42694ad7f6d3a240068c59e57dba9e3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-23 17:09:26 +0400

    Create UnitechApplicationTests.java

---
+++ src/test/java/com/rustam/unitech/UnitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustam.unitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
