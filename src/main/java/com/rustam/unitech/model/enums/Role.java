ffd82ec7ef0f09acbb9fc07dfa82222fe7990364
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-23 17:08:00 +0400

    Create Role.java

---
+++ src/main/java/com/rustam/unitech/model/enums/Role.java
@@ -0,0 +1,24 @@
+package com.rustam.unitech.model.enums;
+
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+public enum Role implements GrantedAuthority {
+    ROLE_USER("USER"),
+    ROLE_ADMIN("ADMIN"),
+    ROLE_MOD("MOD");
+
+    private final String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+
+}
