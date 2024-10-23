37edd453f349cc3694f7af780e7aa8470eb3cd0a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-23 17:06:56 +0400

    Create UserRequest.java

---
+++ src/main/java/com/rustam/unitech/dto/request/UserRequest.java
@@ -0,0 +1,19 @@
+package com.rustam.unitech.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequest {
+
+    private String name;
+
+    private String username;
+
+    private String password;
+}
