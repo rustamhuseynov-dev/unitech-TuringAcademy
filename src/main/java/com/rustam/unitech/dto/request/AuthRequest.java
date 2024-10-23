ad92d7e27d7b0d1fa677b3fc480b6a4d68dd196f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-23 17:06:52 +0400

    Create AuthRequest.java

---
+++ src/main/java/com/rustam/unitech/dto/request/AuthRequest.java
@@ -0,0 +1,19 @@
+package com.rustam.unitech.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
